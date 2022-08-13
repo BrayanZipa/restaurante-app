7017e13eb36ea5e836e94083189db8973d1bd8cc
Brayan Zipa <zipa.fonseca@gmail.com>
2022-08-12 23:45:49 -0500

    Cambios

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'usuarios';
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +23,8 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    protected $primaryKey = 'idUsuarios';
 
     /**
      * The attributes that should be hidden for serialization.
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/productos', [App\Http\Controllers\ProductoController::class, 'index'])->name('productos');
+Route::post('/productos/crear', [App\Http\Controllers\ProductoController::class, 'store'])->name('crearProductos');
+Route::get('/productos/mostrar/{id}', [App\Http\Controllers\ProductoController::class, 'show'])->name('mostrarProducto');
+Route::put('/productos/actualizar/{id}', [App\Http\Controllers\ProductoController::class, 'update'])->name('actualizarProducto');
+Route::delete('/productos/eliminar/{id}', [App\Http\Controllers\ProductoController::class, 'destroy'])->name('eliminarProducto');
